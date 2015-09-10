aa332ebcfa7c7b2e1e8104c849dcb49ebd62c429
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-09-10 17:23:54 +0200

    LPS-58504 Add portlet decorator class to portlet wrapper and make portlet decorator Id avaliable in templates through portletDisplay

--- portal-service/src/com/liferay/portal/theme/PortletDisplay.java
+++ portal-service/src/com/liferay/portal/theme/PortletDisplay.java
@@ -70,6 +70,7 @@ public class PortletDisplay implements Serializable {
 		_modePrint = master.isModePrint();
 		_modeView = master.isModeView();
 		_namespace = master.getNamespace();
+		_portletDecoratorId = master.getPortletDecoratorId();
 		_portletName = master.getPortletName();
 		_portletSetup = master.getPortletSetup();
 		_portletToolbar = master.getPortletToolbar();
@@ -138,6 +139,7 @@ public class PortletDisplay implements Serializable {
 		slave.setModePrint(_modePrint);
 		slave.setModeView(_modeView);
 		slave.setNamespace(_namespace);
+		slave.setPortletDecoratorId(_portletDecoratorId);
 		slave.setPortletName(_portletName);
 		slave.setPortletResource(_portletResource);
 		slave.setPortletSetup(_portletSetup);
@@ -234,6 +236,10 @@ public class PortletDisplay implements Serializable {
 
 	public String getNamespace() {
 		return _namespace;
+	}
+
+	public String getPortletDecoratorId() {
+		return _portletDecoratorId;
 	}
 
 	public <T> T getPortletInstanceConfiguration(Class<T> clazz)
@@ -651,6 +657,10 @@ public class PortletDisplay implements Serializable {
 		_namespace = namespace;
 	}
 
+	public void setPortletDecoratorId(String portletDecoratorId) {
+		_portletDecoratorId = portletDecoratorId;
+	}
+
 	public void setPortletName(String portletName) {
 		_portletName = portletName;
 	}
@@ -874,6 +884,7 @@ public class PortletDisplay implements Serializable {
 	private boolean _modePrint;
 	private boolean _modeView;
 	private String _namespace = StringPool.BLANK;
+	private String _portletDecoratorId = StringPool.BLANK;
 	private String _portletName = StringPool.BLANK;
 	private String _portletResource = StringPool.BLANK;
 	private PortletPreferences _portletSetup;
