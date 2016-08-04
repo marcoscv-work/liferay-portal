f7f05466150b12eafaa07bf5ca29cf0317a0ab29
antoniopol06 <antonio.pol@liferay.com>
2016-08-04 13:52:35 +0200

    SF

--- modules/apps/foundation/frontend-theme/frontend-theme-porygon/src/js/main.js
+++ modules/apps/foundation/frontend-theme/frontend-theme-porygon/src/js/main.js
@@ -2,8 +2,6 @@
 	AUI().ready(
 		'liferay-sign-in-modal',
 		function(A) {
-			var BODY = A.getBody();
-
 			var signIn = A.one('.sign-in > a');
 
 			if (signIn && signIn.getData('redirect') !== 'true') {
@@ -14,7 +12,7 @@
 
 	require(
 		'metal-dom/src/dom',
-		function (domModule) {
+		function(domModule) {
 			var dom = domModule.default;
 
 			var topSearch = function() {
@@ -29,27 +27,39 @@
 				this.searchIcon.setAttribute('data-displayclick', 'hidden');
 
 				if (this.searchInput && this.searchIcon) {
-					dom.on(this.searchIcon, 'click', function(event) {
-						if (instance.searchIcon.getAttribute('data-displayclick') === 'display') {
-							instance.hideInputSearch();
+					dom.on(
+						this.searchIcon,
+						'click',
+						function(event) {
+							if (instance.searchIcon.getAttribute('data-displayclick') === 'display') {
+								instance.hideInputSearch();
+							}
+							else {
+								instance.showInputSearch();
+							}
 						}
-						else {
-							instance.showInputSearch();
-						}
-					});
+					);
 
-					dom.on(this.searchInput, 'keydown', function(event) {
-						if (event.keyCode === 27) {
-							instance.hideInputSearch();
+					dom.on(
+						this.searchInput,
+						'keydown',
+						function(event) {
+							if (event.keyCode === 27) {
+								instance.hideInputSearch();
+							}
 						}
-					})
+					);
 
-					dom.on(this.searchInput, 'blur', function(event) {
-						if (!instance.searchInput.value || instance.searchInput.value === '') {
-							instance.searchIcon.setAttribute('data-displayclick', 'display');
-							instance.hideInputSearch();
+					dom.on(
+						this.searchInput,
+						'blur',
+						function(event) {
+							if (!instance.searchInput.value || instance.searchInput.value === '') {
+								instance.searchIcon.setAttribute('data-displayclick', 'display');
+								instance.hideInputSearch();
+							}
 						}
-					});
+					);
 				}
 			};
 
@@ -68,7 +78,7 @@
 					dom.addClasses(this.search, 'focus');
 					this.searchIcon.setAttribute('data-displayclick', 'display');
 				}
-			}
+			};
 
 			new topSearch();
 		}
