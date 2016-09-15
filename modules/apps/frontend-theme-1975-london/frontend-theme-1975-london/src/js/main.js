e2ff14809b340b60fee614c3ded8851c3c3c9061
antoniopol06 <antonio.pol@liferay.com>
2016-09-15 16:54:57 +0200

    ES2015 JS 1975 london theme

--- modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/src/js/main.js
+++ modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/src/js/main.js
@@ -1,23 +1,32 @@
-AUI().ready(
-	'liferay-sign-in-modal',
-	function(A) {
-		var BODY = A.getBody();
+(function() {
+	AUI().ready(
+		'liferay-sign-in-modal',
+		function(A) {
+			var BODY = A.getBody();
 
-		var signIn = A.one('#sign-in');
+			var signIn = A.one('#sign-in');
 
-		if (signIn && signIn.getData('redirect') !== 'true') {
-			signIn.plug(Liferay.SignInModal);
+			if (signIn && signIn.getData('redirect') !== 'true') {
+				signIn.plug(Liferay.SignInModal);
+			}
+
+			var fullScreenToggleIcon = A.one('.full-screen-navigation #banner .navbar-header .navbar-toggle');
+
+			if (fullScreenToggleIcon) {
+				fullScreenToggleIcon.on(
+					'click',
+					function(event) {
+						BODY.toggleClass('main-nav-opened', event.currentTarget.hasClass('collapsed'));
+					}
+				);
+			}
 		}
+	);
 
-		var fullScreenToggleIcon = A.one('.full-screen-navigation #banner .navbar-header .navbar-toggle');
-
-		if (fullScreenToggleIcon) {
-			fullScreenToggleIcon.on(
-				'click',
-				function(event) {
-					BODY.toggleClass('main-nav-opened', event.currentTarget.hasClass('collapsed'));
-				}
-			);
+	require(
+		'1975-london-theme/js/music_player.es',
+		function(MusicPlayer) {
+			new MusicPlayer.default();
 		}
-	}
-);
+	);
+})();
