9499a892ad2108220a5f80ae59e4559ead85af12
edalgrin <eduardo.allegrini@liferay.com>
2021-09-15 16:19:43 +0200

    LPS-134071 Replace .hide-accessible with .sr-only

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/asset_categories_selector.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/asset_categories_selector.js
@@ -769,7 +769,7 @@ AUI.add(
 
 					instance._renderIcons();
 
-					instance.inputContainer.addClass('hide-accessible');
+					instance.inputContainer.addClass('sr-only');
 
 					instance._applyARIARoles();
 				},
