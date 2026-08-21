2a8682d42ba7171e1426563d6ef5030c96687662
marcoscv-work <marcos.castro@liferay.com>
2026-08-21 12:19:08 +0200

    LPD-102667 Core: add the parametric edit state and imaging engine

--- modules/apps/frontend-js/frontend-js-image-editor-web/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/frontend-js/frontend-js-image-editor-web/src/main/resources/META-INF/resources/js/index.ts
@@ -3,4 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export {};
+export type {EditorConfig} from './editorConfig';
+export type {EditorMessages} from './i18n';
+export {setMessages} from './i18n';
+export {liferayMessages} from './i18n/liferayMessages';
+export type {ImageLoadErrorReason, LoadedImage} from './imaging/loadImage';
+export {
+	ImageEditorLoadError,
+	disposeLoadedImage,
+	loadImage,
+} from './imaging/loadImage';
+
+// EditState is public because EditorSaveResult carries one.
+
+export type {EditState} from './state/types';
